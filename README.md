d55e7aef021883d76a8376c35afc668fbf83b572
심승용 <seungyongshim@nexon.co.kr>
2022-02-03 18:13:42 +0900

    fsharp이어도 멱등 문제 있

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ![CI](../../workflows/CI/badge.svg)
+
+* https://github.com/Arshia001/FSharp.GrpcCodeGenerator
---
+++ src/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using ClusterHelloWorld.Messages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FSharp.Core;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var dto = new HelloResponse(null, "Hello");
+
+            dto.Message = "What!";
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
